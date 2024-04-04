b8daf411ee78adcce495f3e07ede10ef48ec500e
ArthurShelter <158783244+ArthurShelter@users.noreply.github.com>
2024-04-04 13:59:05 -0700

    Finishing to-do list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,30 @@
 function newItem() {
-    alert("Button is clicked" );
-    };
+    let li = $("<li></li>");
+    let inputValue = $("#input").val();
+    li.append(inputValue);
+
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        $("#list").append(li);
+    }
+
+    //2. Crossing out an item from the list of items:
+    function crossOut() {
+        li.toggleClass("strike");
+    }
+
+    li.on("dblclick", crossOut);
+    //3(i) Adding delete button in the form of an X.
+    let deleteButton = $("<deleteButton></deleteButton>").append(document.createTextNode("X"));
+    li.append(deleteButton);
+
+    deleteButton.on("click", deleteListItem);
+
+    //3(ii) Adding CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem() {
+        li.addClass("delete")
+    }
+    // 4. Reordering the items: 
+    $("#list").sortable();
+}
